5e62d459847d035aedc4a9556cb1f373b3713067
Joaovitor961 <joaovitorgarciamoreira@gmail.com>
2025-10-28 10:32:52 -0300

    importação do javascript do bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/main.scss'   // seu SCSS (importa bootstrap ali também)
+import './styles/main.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'  // JavaScript do Bootstrap
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
